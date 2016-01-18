9a17356affdaba561acb67dedf2319b3b784b3b8
Tim Morgan <tim@timmorgan.org>
2016-01-17 19:41:24 -0600

    Add an action to delete posts
    Create another action in the PostsController:
    
        def destroy
          @post = Post.find(params[:id])
          @post.destroy
          redirect_to action: :index
        end
    
    Now let's add a button to our view:
    
        <p class="pull-right">
          <%= link_to post, data: { method: 'delete', confirm: 'Are you sure?' }, class: 'btn btn-xs btn-danger' do %>
            <i class='glyphicon glyphicon-trash'></i>
            delete
          <% end %>
        </p>
    
    That's it! Now you can delete posts (and so can anyone else).

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,12 @@ class PostsController < ApplicationController
     redirect_to action: :index
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to action: :index
+  end
+
   private
 
   def post_params
